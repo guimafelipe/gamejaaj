28b29663c299a418d063d6acf046b366499125a2
Felipe Guimaraes <guima.felipec@gmail.com>
2018-07-18 19:07:59 -0300

    added some comments

--- Assets/Ponteiro/Ponteiro.gd
+++ Assets/Ponteiro/Ponteiro.gd
@@ -2,6 +2,8 @@ extends Spatial
 
 const Player = preload('res://Assets/Gugu/Gugu.gd')
 const UP = Vector3(0,1,0)
+
+#Basta herdar dessa classe e mudar as constantes
 const angular_speed = 2*PI/3
 const dmg = 1
 
--- Assets/Ponteiro/Ponteiro2.gd
+++ Assets/Ponteiro/Ponteiro2.gd
@@ -1,7 +1,3 @@
 extends "res://Assets/Ponteiro/Ponteiro.gd"
 
 const angular_speed = 2*PI/6
-
-func _ready():
-	pass
-
