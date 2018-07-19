782e9fa0c870d32a9703d2e0acba17e9891a32c8
Felipe Guimaraes <guima.felipec@gmail.com>
2018-07-19 20:23:47 -0300

    começando refatoração

--- Assets/Ponteiro/Ponteiro2.gd
+++ Assets/Ponteiro/Ponteiro2.gd
@@ -1,3 +1,1 @@
 extends "res://Assets/Ponteiro/Ponteiro.gd"
-
-const angular_speed = 2*PI/6
--- Teste/TestMap.gd
+++ Teste/TestMap.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
 func _ready():
-	var center_pos = $Centro.global_transform.origin
+	var center_pos = $Ambient/Centro.global_transform.origin
 	$Gugu.set_center(center_pos)
 	$BasePivot.set_center(center_pos)
 	$BasePivot.set_player($Gugu)
