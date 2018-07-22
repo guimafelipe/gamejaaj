bfc44ff1fd964a3e896a8efc618e58ead51a7fad
Felipe Guimaraes <guima.felipec@gmail.com>
2018-07-22 15:52:52 -0300

    refactored nodes and some code

--- Assets/GeneralScripts/LevelManager.gd
+++ Assets/GeneralScripts/LevelManager.gd
@@ -4,17 +4,27 @@ onready var player = $Gugu
 onready var center = $Ambient/Centro.global_transform.origin
 onready var player_spawn = $PlayerSpawn.global_transform.origin
 
+# Timers
+onready var countdown_timer = $Timers/CountdownTimer
+onready var level_timer = $Timers/LevelTimer
+onready var winanim_timer = $Timers/WinAnimTimer
+onready var loseanim_timer = $Timers/LoseAnimTimer
+
+
 func _ready():
 	# This node has all below dependencies to work
 	player.set_center(center)
 	$BasePivot.set_center(center)
 	$BasePivot.set_player(player)
-	$CountdownTimer.connect("timeout", self, "on_countdown_ended")
-	$LevelTimer.connect("timeout", self, "level_win")
-	$WinAnimTimer.connect("timeout", self, "on_win_anim_ended")
-	$LoseAnimTimer.connect("timeout", self, "on_lose_anim_ended")
+	connect_timers()
 	player.connect("died", self, "on_player_died")
 	start_level()
+
+func connect_timers():
+	countdown_timer.connect("timeout", self, "on_countdown_ended")
+	level_timer.connect("timeout", self, "level_win")
+	winanim_timer.connect("timeout", self, "on_win_anim_ended")
+	loseanim_timer.connect("timeout", self, "on_lose_anim_ended")
 
 func start_level():
 	# call only once per level play
@@ -22,8 +32,8 @@ func start_level():
 
 func restart_level():
 	level_setup()
-	$CountdownTimer.start()
-	$UI.set_countdown_timer($CountdownTimer)
+	countdown_timer.start()
+	$UI.set_countdown_timer(countdown_timer)
 	# 3 seconds countdown here
 
 func restart_pointers():
@@ -44,32 +54,28 @@ func on_cutscene_ended():
 	$Cutscene/CutsceneAnim.stop()
 	level_setup()
 	$UI.enter_gameplay()
-	$CountdownTimer.start()
-	$UI.set_countdown_timer($CountdownTimer)
+	countdown_timer.start()
+	$UI.set_countdown_timer(countdown_timer)
 
 func level_win():
 	player.can_move = false
 	for pointer in $Ponteiros.get_children():
 		pointer.stop_movement()
-	$WinAnimTimer.start()
+	winanim_timer.start()
 	# call next level
 
-func cutscene_ended():
-	pass
-	# start game here
-
 func on_player_died():
-	$LevelTimer.stop()
+	level_timer.stop()
 	for pointer in $Ponteiros.get_children():
 		pointer.stop_movement()
 	# wait 2 seconds maybe
-	$LoseAnimTimer.start()
+	loseanim_timer.start()
 
 func on_countdown_ended():
 	restart_pointers()
 	player.liberate_movement()
-	$LevelTimer.start()
-	$UI.set_survive_timer($LevelTimer)
+	level_timer.start()
+	$UI.set_survive_timer(level_timer)
 
 func on_lose_anim_ended():
 	restart_level()
