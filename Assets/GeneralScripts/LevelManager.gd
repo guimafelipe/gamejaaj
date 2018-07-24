089e1cfc86b6912fd7214669a5da9d8fa1f037df
Felipe Guimaraes <guima.felipec@gmail.com>
2018-07-24 01:00:55 -0300

    added pause system

--- Assets/GeneralScripts/LevelManager.gd
+++ Assets/GeneralScripts/LevelManager.gd
@@ -83,3 +83,8 @@ func on_lose_anim_ended():
 
 func on_win_anim_ended():
 	print("win anim ended")
+
+func _process(delta):
+	if(Input.is_action_just_pressed("pause")):
+		get_tree().paused = true
+		$PauseUI.show()
---
+++ Assets/UI/PauseUI/PauseUI.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_ResumeBtn_button_up():
+	hide()
+	get_tree().paused = false
+
+func _on_LevelSelectBtn_button_up():
+	pass # replace with function body
+
+func _on_MainMenuBtn_button_up():
+	pass # replace with function body
